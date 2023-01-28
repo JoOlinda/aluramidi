b8b93e87c65dc44e8c1ce2c883e90e2ce1eaa0fc
Joeldo Olinda <joeldo.olinda@gmail.com>
2023-01-28 19:38:55 -0300

    Refatoraão para executar todos  os sons

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-function tocaSom(){
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom(idTeclaSom){
+    document.querySelector(idTeclaSom).play()
 }
+
 const listaTeclas = document.querySelectorAll('.tecla')
 let i = 0
+
 while (i < listaTeclas.length) {
-    listaTeclas[i].onclick = tocaSom
+    const tecla = listaTeclas[i]
+    const idInstrumento = `#som_${tecla.classList[1]}`
+    tecla.onclick = function(){
+        tocaSom(idInstrumento)
+    }
     i =i+1
 }
